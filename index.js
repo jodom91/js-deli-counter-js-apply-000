b6ad8fd00e0975cfd01e9f818a2fe55cf7fd6db9
John Odom <jodom91@gmail.com>
2019-06-04 19:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-      function takeANumber(actualLine, newPerson) {
-
-          actualLine.push(newPerson);
-          var lineLength = actualLine.length;
-          var welcomePhrase = "Welcome, " + newPerson + ". You are number " + lineLength + " in line.";
+// should return "Welcome, newPerson, you are number (# back of line) in line"
+//"Welcome, Ada. You are number 1 in line."
+// this is if somebody new walks into the store
+var actualLine = [];
+var i=1 
+      function takeANumber(actualLine) {
+          
+          actualLine.push(i++); //[1] i=2
+          var welcomePhrase = ". You are number " + i-1; //You are number 1
           return(welcomePhrase);
-            
       }  
+      
+actualLine = [0]
+      
+  // accepts an array and returns first person in that array so they can be served.
+  // if nobody is in the array, returns"There is nobody waiting to be served!"
+  // kicks off first element in array.
+  
       function nowServing(katzDeliLine) { 
           if(katzDeliLine.length>0) {
           var firstInLine = "Currently serving " + katzDeliLine[0] + ".";
@@ -15,6 +25,11 @@
           return "There is nobody waiting to be served!"
       }    
       }
+      
+      //accepts an array and returns the array as a String
+      //"The line is currently..."
+      //if nobody in line, return "the line is currently empty"
+      
       function currentLine(katzDeliLine) {
           var i = 0;
           var lineLength = "The line is currently: ";
